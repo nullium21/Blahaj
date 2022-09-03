99ffd4b7758d24e8f5254df3f726c93f4f104f52
nullium21 <rommyskovets@gmail.com>
2022-09-03 20:52:35 +0300

    Port mod to Fabric

--- src/main/java/hibi/blahaj/Common.java
+++ src/main/java/hibi/blahaj/Common.java
@@ -1,24 +1,18 @@
 package hibi.blahaj;
 
-import org.quiltmc.loader.api.ModContainer;
-
+import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class Common {
+public class Common implements ModInitializer {
 
-	public static final Identifier BLAHAJ_ID;
-	public static final Identifier BREAD_ID;
+	public static final Identifier BLAHAJ_ID = new Identifier("blahaj", "blue_shark");
+	public static final Identifier BREAD_ID = new Identifier("blahaj", "bread");
 
-	public void onInitialize(ModContainer mod) {
+	public void onInitialize() {
 		Registry.register(Registry.ITEM, BLAHAJ_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC), "item.blahaj.blue_shark.tooltip"));
 		Registry.register(Registry.ITEM, BREAD_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC), null));
-	}
-
-	static {
-		BLAHAJ_ID = new Identifier("blahaj", "blue_shark");
-		BREAD_ID = new Identifier("blahaj", "bread");
 	}
 }
